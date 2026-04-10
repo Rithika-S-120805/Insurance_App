9e02285d808e0740e2821a82d55fce93b0f6b522
RITHIKA SENTHILKUMAR <s.rithika120805@gmail.com>
2026-04-10 14:21:24 +0530

    second

--- src/main/java/com/kgisl/pos/repository/CustomerRepository.java
+++ src/main/java/com/kgisl/pos/repository/CustomerRepository.java
@@ -4,4 +4,4 @@ import com.kgisl.pos.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
+} 
